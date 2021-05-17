06cc3824a563a1e9443ec4c5458fb0d93757afd3
hhimanshu <harit.himanshu@yahoo.in>
2021-05-17 16:15:30 -0700

    feat: add documentation on how to use msw generated by orval

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,4 +1,15 @@
 import { AUTH_HANDLERS } from './auth';
 import { BUSINESS_HANDLERS } from './businesses';
+// import { getDefaultMSW } from '../../openapi/generated/default/default.msw';
 
 export const handlers = [...AUTH_HANDLERS, ...BUSINESS_HANDLERS];
+
+/**
+ * The fake data from orval is not qualitative, and complete for the needs
+ * of our application. When I say complete, it misses the fields necessary for us to see the data.
+ * I prefer to take control of the mocked data so that when working, the
+ * data is closer to the domain. Hence I will comment the following line,
+ * and continue to use the mocked data prepared by myself.
+ * The following line gives an example on how to use faker data with MSW when using orval.
+ */
+// export const handlers = [...getDefaultMSW()];
